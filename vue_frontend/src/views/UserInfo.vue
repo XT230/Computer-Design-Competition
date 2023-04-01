b7ff1ade28c0b508332dcf6a9709711741b11e40
AC-Accelerator <dengyuzhe.personal@gmail.com>
2023-04-01 21:52:04 +0800

    个人信息板块

--- vue_frontend/src/views/UserInfo.vue
+++ vue_frontend/src/views/UserInfo.vue
@@ -1,11 +1,110 @@
 <template>
-  
+    <el-descriptions class="margin-top" title="我的信息" :column="1" size="large" border>
+        <el-descriptions-item>
+            <template #label>
+                <div class="cell-item">
+                    <el-icon :style="iconStyle">
+                        <user />
+                    </el-icon>
+                    昵称
+                </div>
+            </template>
+            {{ userID }}
+        </el-descriptions-item>
+        <el-descriptions-item>
+            <template #label>
+                <div class="cell-item">
+                    <el-icon :style="iconStyle">
+                        <iphone />
+                    </el-icon>
+                    电话
+                </div>
+            </template>
+            {{ userPhone }}
+        </el-descriptions-item>
+
+        <el-descriptions-item>
+            <template #label>
+                <div class="cell-item">
+                    <el-icon :style="iconStyle">
+                        <tickets />
+                    </el-icon>
+                    学校
+                </div>
+            </template>
+            {{ userSchool }}
+        </el-descriptions-item>
+        <el-descriptions-item>
+            <template #label>
+                <div class="cell-item">
+                    <el-icon :style="iconStyle">
+                        <office-building />
+                    </el-icon>
+                    邮箱
+                </div>
+            </template>
+            {{ userMail }}
+        </el-descriptions-item>
+    </el-descriptions>
 </template>
 
-<script lang="ts" setup>
+<script lang="ts">
+import { computed, ref } from 'vue'
+import {
+    Iphone,
+    Location,
+    OfficeBuilding,
+    Tickets,
+    User,
+} from '@element-plus/icons-vue'
 
+const size = ref('')
+
+import { defineComponent } from 'vue'
+export default defineComponent({
+    data() {
+        return {
+            userAvatar: 'https://cube.elemecdn.com/3/7c/3ea6beec64369c2642b92c6726f1epng.png',
+            userID: "大家好我是说的道理",
+            userSchool: "东北大学秦皇岛分校",
+            userPhone: "18100000000",
+            userMail: "wtf",
+            iconStyle: computed(() => {
+                const marginMap = {
+                    large: '8px',
+                    default: '6px',
+                    small: '4px',
+                }
+                return {
+                    marginRight: marginMap.default,
+                }
+            }),
+            blockMargin: computed(() => {
+                const marginMap = {
+                    large: '32px',
+                    default: '28px',
+                    small: '24px',
+                }
+                return {
+                    marginTop: marginMap.default,
+                }
+            })
+        }
+    }
+})
 </script>
 
-<style>
+<style scoped>
+.el-descriptions {
+    margin-top: 20px;
+}
 
+.cell-item {
+    display: flex;
+    align-items: center;
+}
+
+.margin-top {
+    margin-top: 20px;
+}
 </style>
